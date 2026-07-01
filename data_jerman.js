60a98ee8dbb91a10f74da8b188d5cf8ca97cc107
Vylonium <vylonium@gmail.com>
2026-07-01 09:58:35 +0700

    feat(jerman): init data_jerman.js placeholder

---
+++ data_jerman.js
@@ -0,0 +1,9 @@
+(function() {
+  if (typeof window.countryDataFiles === 'undefined') window.countryDataFiles = {};
+  window.countryDataFiles['jerman'] = {
+    names: { id: 'Jerman', en: 'Germany', ja: 'ドイツ', ko: '독일', zh: '德国' },
+    flag: '🇩🇪',
+    articles: []
+  };
+  window.countries = window.countryDataFiles;
+})();
