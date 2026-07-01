aef8417518fe28438a3d64607b11da94163f1a78
Vylonium <vylonium@gmail.com>
2026-07-01 09:58:31 +0700

    feat(mesir): init data_mesir.js placeholder

---
+++ data_mesir.js
@@ -0,0 +1,9 @@
+(function() {
+  if (typeof window.countryDataFiles === 'undefined') window.countryDataFiles = {};
+  window.countryDataFiles['mesir'] = {
+    names: { id: 'Mesir', en: 'Egypt', ja: 'エジプト', ko: '이집트', zh: '埃及' },
+    flag: '🇪🇬',
+    articles: []
+  };
+  window.countries = window.countryDataFiles;
+})();
