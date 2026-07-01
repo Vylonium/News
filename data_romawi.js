f27c555f3bd24e1935a860548fa1ee5d78a8d204
Vylonium <vylonium@gmail.com>
2026-07-01 09:58:32 +0700

    feat(romawi): init data_romawi.js placeholder

---
+++ data_romawi.js
@@ -0,0 +1,9 @@
+(function() {
+  if (typeof window.countryDataFiles === 'undefined') window.countryDataFiles = {};
+  window.countryDataFiles['romawi'] = {
+    names: { id: 'Italia', en: 'Italy', ja: 'イタリア', ko: '이탈리아', zh: '意大利' },
+    flag: '🇮🇹',
+    articles: []
+  };
+  window.countries = window.countryDataFiles;
+})();
