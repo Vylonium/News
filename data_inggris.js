001fc416fd5b565baa625f3f8c8f80cc2c9e6394
Vylonium <vylonium@gmail.com>
2026-07-01 09:58:34 +0700

    feat(inggris): init data_inggris.js placeholder

---
+++ data_inggris.js
@@ -0,0 +1,9 @@
+(function() {
+  if (typeof window.countryDataFiles === 'undefined') window.countryDataFiles = {};
+  window.countryDataFiles['inggris'] = {
+    names: { id: 'Inggris', en: 'UK', ja: 'イギリス', ko: '영국', zh: '英国' },
+    flag: '🇬🇧',
+    articles: []
+  };
+  window.countries = window.countryDataFiles;
+})();
